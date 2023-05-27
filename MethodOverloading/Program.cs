6e98d99a86651d9e76d723f476a6d9dea27e2c72
saulpadilla10 <saulp1021@yahoo.com>
2023-05-27 13:18:17 -0700

    Add Overload

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,57 @@
 {
     public class Program
     {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool z)
+        {
+            var b = x + y;
+
+            if (z == true && b == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else if (z == true && b > 1)
+            {
+                return $"{b} dollars";
+            }
+            else if (z == true && b < 1)
+            {
+                return $"{b} dollars";
+            }
+
+            else
+            {
+                return b.ToString();
+            }
+        }
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var x = 45;
+            var y = 67;
+
+            var answer = Add(x, y);
+
+            var h = 0.43m;
+            var k = 3.45m;
+
+            var decimalTurn = Add(h, k);
+
+            var thirdTurn = Add(0, 1, true);
+
+            Console.WriteLine($"add: {answer} decimal add: {decimalTurn}");
+
+            Console.WriteLine($"{thirdTurn}");
         }
     }
 }
